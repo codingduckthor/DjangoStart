433db5679f4694a23410f1e0922d7e6e6ff50a50
Nail <rolla1n1rock@gmail.com>
2025-11-21 18:26:32 +0500

    lesson_02

--- bboard/views.py
+++ bboard/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
+from bboard.models import Bb
+from django.shortcuts import render
 
 # from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, bober")
+    # template = loader.get_template('index.html')
+    bbs = Bb.objects.all()
+    context = {'bbs': bbs}
+
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', context)
